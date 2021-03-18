573646c9a096bb7669d365880bf19dc44938e933
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-18 18:06:46 -0300

    ninja3 atualização

--- ninja3/ninja3.go
+++ ninja3/ninja3.go
@@ -8,6 +8,10 @@ type ninja int
 
 var x ninja
 
+//utilizando a palavra chave var, criando uma variável com o identificador y
+var y int
+
+//package-leve scope
 func main() {
 	//demonstra o valor da variável x
 	fmt.Println(x)
@@ -15,6 +19,14 @@ func main() {
 	fmt.Printf("%T\n", x)
 	//atribui 42 a variável x utilizando o operador de atribuição
 	x = 42
+	//demonstra o valor de x
 	fmt.Println(x)
+	//utilização de conversão para transformar o tipo do valor da variável "x" em seu subjacente e, utilizando o operador "="
+	//atribui o valor y ao x
+	y = int(x)
+	//valor de x
+	fmt.Println(x)
+	//tipo de x
+	fmt.Printf("%T\n", x)
 
 }
